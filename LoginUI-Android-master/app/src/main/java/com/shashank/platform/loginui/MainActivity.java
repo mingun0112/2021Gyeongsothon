93c2a07bbf29c16891062985cc2312e7622b8d52
mingun0112 <mingun0112@naver.com>
2021-10-22 15:56:15 +0900

    loginui

---
+++ LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/MainActivity.java
@@ -0,0 +1,44 @@
+package com.shashank.platform.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    int count = 0;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
+            public void onSwipeTop() {
+            }
+
+            public void onSwipeRight() {
+
+            }
+
+            public void onSwipeLeft() {
+
+            }
+
+            public void onSwipeBottom() {
+            }
+
+        });
+    }
+}
