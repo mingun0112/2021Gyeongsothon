f1b6e33ff460043c555c8a796c45645b40f39e06
kimyeonhong00 <63278864+kimyeonhong00@users.noreply.github.com>
2021-10-26 12:18:39 +0900

    update

---
+++ LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/MainActivity_chat.java
@@ -0,0 +1,35 @@
+package com.shashank.platform.loginui;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;*/
+
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity_chat extends AppCompatActivity{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Chat> chatList;
+    private String nickname = "익명1";
+
+    private EditText chatText;
+    private Button sendButton;
+
+
+
+}
+
+
