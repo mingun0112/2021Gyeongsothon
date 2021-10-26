4d0fcf5671fd4ba7676fbe35bfc23c9d31405b64
mingun0112 <mingun0112@naver.com>
2021-10-26 15:01:09 +0900

    map

--- LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/MainActivity.java
+++ LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
         });
