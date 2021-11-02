6f745c16586affce978e2f3385d52f4ee8250932
kimyeonhong00 <63278864+kimyeonhong00@users.noreply.github.com>
2021-11-02 14:56:26 +0900

    up

--- LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/RegisterActivity.java
+++ LoginUI-Android-master/app/src/main/java/com/shashank/platform/loginui/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.shashank.platform.loginui;
 
+<<<<<<< Updated upstream
 import android.support.annotation.NonNull;
 import android.support.v4.app.AppCompatActivity;
+=======
+
+import android.support.v4.app.*;
+>>>>>>> Stashed changes
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +14,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+<<<<<<< Updated upstream
+=======
+import androidx.annotation.NonNull;
+>>>>>>> Stashed changes
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
